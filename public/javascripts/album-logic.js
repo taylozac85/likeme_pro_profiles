350057c45e3579ed58980439de088659453c6228
taylozac85 <taylozac85@gmail.com>
2013-07-27 22:03:05 -0700

    dynamic resizing of images

--- public/javascripts/album-logic.js
+++ public/javascripts/album-logic.js
@@ -5,7 +5,7 @@ $(function(){
 
 		$('#pro-album-1').show();
 		var album_cover = album.images[0];
-		$('#pro-album1-cover').attr('src', album_cover);
+		$('#pro-album1-cover').attr('src', "images/coffee1-8.jpg");
 
 		$('#pro-album-1').on('click', function(){
       var $focusPhoto = $('#profile-modal-large-img');
@@ -42,4 +42,44 @@ $(function(){
     	};
 		});
 	};
+
+	function getImgSize(imgSrc) {
+		var newImg = new Image();
+
+		newImg.onload = function() {
+		var height = newImg.height;
+		var width = newImg.width;
+		}
+
+		newImg.src = imgSrc; // this must be done AFTER setting onload
+	};
+
+	(function() {
+		var newImg = new Image();
+		imgSrc = "images/coffee1-8.jpg";
+
+		newImg.onload = function() {
+			var height = newImg.height;
+			var width = newImg.width;
+			console.log('The image size is '+width+'*'+height);
+			
+			var $album_cover = $('#pro-album1-cover');
+			if (width / height < 330 / 250) {
+				$album_cover.css('width', '330');
+				$album_cover.css('height', 'auto');
+				$album_cover.css('top', '50%');
+				$album_cover.css('margin-top', '-60%');
+			}
+		}
+
+		newImg.src = imgSrc; // this must be done AFTER setting onload
+
+		
+
+		
+		
+	
+	}).call(this);
+	
+
 });
