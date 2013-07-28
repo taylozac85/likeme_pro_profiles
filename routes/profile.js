0e14be8a5ccb7f68061c823dfb469826bc515291
Nick Taylor <nick.taylor1207@gmail.com>
2013-07-27 18:35:10 -0700

    middleware and change pro-profile

--- routes/profile.js
+++ routes/profile.js
@@ -1,15 +1,22 @@
+var Album = require('../data/models/albums');
+var loggedIn = require('./middleware/logged_in');
+
 module.exports = function(app) {
 
   app.get('/edit-profile', function(req, res){
   	res.render('edit-profile');
   });
 
-  app.get('/pro-profile', function(req, res){
-  	if (req.session.user) {
-  		res.render('pro-profile', {user: req.session.user});
-  	} else {
-  		res.redirect('login');
-  	};
+  app.get('/pro-profile', loggedIn, function(req, res, next){
+    var instance = req.session.user.email;
+    Album.findOne({ 'user_email' : instance }, function(err, album){
+      if (!album){
+        console.log("No Album");
+        res.render('pro-profile', {user: req.session.user});
+      } else {
+        console.log(album.images);
+    		res.render('pro-profile', {user: req.session.user, album: album});
+      }
+    });
   });
-
 };
