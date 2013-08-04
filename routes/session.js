a2fb33898aeb3168896fe0d9896a4859d414b5d7
Nick Taylor <nick.taylor1207@gmail.com>
2013-08-04 13:12:07 -0700

    delete login and logout files

--- routes/session.js
+++ routes/session.js
@@ -6,6 +6,16 @@ module.exports = function(app) {
   	res.render('login')
   });
 
+  app.get('/login', function(req, res) {
+    res.render('login');
+  });
+
+
+	app.get('/logout', function(req, res) {
+		req.session = null;
+		res.redirect('users/new');
+	});
+
   app.post('/session', function(req, res) {
 		User.findOne({email: req.body.email, password: req.body.password}, function(err, user) {
 			if (err) {
